9ff48b816212c04a212401d942042332ecb7a6d4
cos-overclock <60503661+cos-overclock@users.noreply.github.com>
2023-11-06 11:10:39 +0900

    add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://con-create-development.github.io',
+    base: '/con-create-blog',
+});
